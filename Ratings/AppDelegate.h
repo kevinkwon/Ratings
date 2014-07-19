142aac5017fdf7c816df49a7d8c708ba8ddcd3da
Kevin Kwon <myhhkb@gmail.com>
2014-07-19 10:09:38 +0900

    Initial Commit

---
+++ Ratings/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ratings
+//
+//  Created by hdk on 2014. 7. 19..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by hdk on 2014. 7. 19..
+//  Copyright (c) 2014년 Kevin Kwon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
